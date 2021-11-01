a0b264e7cab24f84c582389456229af730cececc
Vasyl Stolyarchuk <Vasyl.Stolyarchuk@outlook.com>
2021-11-01 21:43:55 +0200

    add locig for delete transaction and add pages

--- src/web/Data/Adapters/TransactionAdapter.cs
+++ src/web/Data/Adapters/TransactionAdapter.cs
@@ -9,20 +9,51 @@ using web.Data.ModelDtos;
 
 namespace web.Data.Adapters
 {
-    public class TransactionAdapter
+    public static class TransactionAdapter
     {
+        public static List<TransactionDto> GetTransaction(string userId)
+        {
+            var result = new List<TransactionDto>();
+
+            string sql = null;
+            sql = string.Format(@"exec [sp_GetTransaction]  {0}",
+            DataBaseHelper.SafeSqlString(userId));
+            var sqlResult = DataBaseHelper.GetSqlResult(sql);
+
+            if (sqlResult.Rows.Count > 0)
+            {
+                foreach (DataRow item in sqlResult.Rows)
+                {
+                    result.Add(new TransactionDto
+                    {
+                        ID = DataBaseHelper.GetIntegerValueFromRowByName(item, "ID"),
+                        Amount = DataBaseHelper.GetDecimalValueFromRowByName(item, "Amount"),
+                        Note = DataBaseHelper.GetValueFromRowByName(item, "Note"),
+                        TransactionDate = DataBaseHelper.GetDateTimeValueFromRowByName(item, "TransactionDate"),
+                        IsHidden = DataBaseHelper.GetBoolValueFromRowByName(item, "IsHidden"),
+                        TransactionTypeID = DataBaseHelper.GetIntegerValueFromRowByName(item, "TransactionTypeID"),
+                        TransactionCategoryID = DataBaseHelper.GetIntegerValueFromRowByName(item, "TransactionCategoryID"),
+                        IsActive = DataBaseHelper.GetBoolValueFromRowByName(item, "IsActive")
+                    });
+                }
+            }
+
+            return result;
+        }
+
+
         public static void SaveTransaction(TransactionDto model)
         {
             var sql = string.Empty;
-            var format = "yyyy-MM-dd HH:mm:ss:fff";
-            var stringDateTransaction = model.TransactionDate.ToString(format);
+            //var format = "yyyy-MM-dd HH:mm:ss:fff";
+            //var stringDateTransaction = model.TransactionDate.ToString(format);
             if (model.ID > 0)
             {
                 sql = string.Format(@"EXEC [sp_SaveTransaction] {0}, {1}, {2} ,{3}, {4}, {5},{6}",
                 DataBaseHelper.RawSafeSqlString(model.ID),
                 DataBaseHelper.RawSafeSqlString(model.Amount),
                 DataBaseHelper.SafeSqlString(model.Note),
-                DataBaseHelper.SafeSqlString(stringDateTransaction),                
+                DataBaseHelper.SafeSqlString(model.TransactionDate),                
                 DataBaseHelper.SafeSqlString(model.User.Id),
                 DataBaseHelper.RawSafeSqlString(model.TransactionCategory.ID),
                 DataBaseHelper.RawSafeSqlString(model.TransactionType.ID));
@@ -33,11 +64,21 @@ namespace web.Data.Adapters
             DataBaseHelper.RawSafeSqlString(model.ID),
             DataBaseHelper.RawSafeSqlString(model.Amount),
             DataBaseHelper.SafeSqlString(model.Note),
-            DataBaseHelper.SafeSqlString(stringDateTransaction),           
+            DataBaseHelper.SafeSqlString(model.TransactionDate),           
             DataBaseHelper.SafeSqlString(model.User.Id),
             DataBaseHelper.RawSafeSqlString(model.TransactionCategory.ID),
             DataBaseHelper.RawSafeSqlString(model.TransactionType.ID));
             DataBaseHelper.RunSql(sql);           
+        }
+
+        public static void DeleteTransaction(int id)
+        {
+            if (id > 0)
+            {
+                string sql = string.Format(@"exec sp_DeleteTransaction {0}",
+                DataBaseHelper.RawSafeSqlString(id));
+                DataBaseHelper.RunSql(sql);
+            }
         }
     }
 }
--- src/web/Data/ModelDtos/TransactionDto.cs
+++ src/web/Data/ModelDtos/TransactionDto.cs
@@ -15,7 +15,13 @@ namespace web.Data.ModelDtos
         public DateTime TransactionDate { get; set; }
         public bool IsRemoved { get; set; }
         public DateTime DateRemoved { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Filter { get; set; }
         public bool IsHidden { get; set; }
+        public int TransactionTypeID { get; set; }
+        public int TransactionCategoryID { get; set; }
+        public bool IsActive { get; set; }
         public virtual IdentityUser User { get; set; }
         public virtual TransactionCategoryDto TransactionCategory { get; set; } = new TransactionCategoryDto();
         public virtual TransactionTypeDto TransactionType { get; set; }
