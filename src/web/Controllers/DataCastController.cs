95b6441a1b817c47c8a660a753cf4c88ac2f66c7
Vasyl Stolyarchuk <Vasyl.Stolyarchuk@outlook.com>
2021-11-06 00:13:43 +0200

    add save in file

---
+++ src/web/Controllers/DataCastController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+
+using Newtonsoft.Json;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+using web.Data.ModelDtos.ModelDataCast;
+
+namespace web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataCastController : ControllerBase
+    {
+        private readonly IHostEnvironment _environment;
+
+        public DataCastController(IHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromForm] string id, [FromForm] string jsonString)
+        {
+            string filepath = string.Empty;
+
+            if (jsonString != null)
+            {
+                var date = DateTime.Now.ToString("dd-mm-yy");
+                string path = Path.Combine(_environment.ContentRootPath, $"wwwroot\\dataCast\\{date}");
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+
+                
+                filepath = $"{path}\\{id}" + ".json";
+               
+
+                System.IO.File.WriteAllText(filepath, jsonString.ToString());
+
+
+                return Ok();
+            }
+
+            return NoContent();
+        }
+    }
+}
