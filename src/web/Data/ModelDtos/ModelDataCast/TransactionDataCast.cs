3a6e8fe31a76632287a22bc28da83001acd76e20
Vasyl Stolyarchuk <Vasyl.Stolyarchuk@outlook.com>
2021-11-05 09:50:13 +0200

    add data cast

---
+++ src/web/Data/ModelDtos/ModelDataCast/TransactionDataCast.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace web.Data.ModelDtos.ModelDataCast
+{   
+    public class TransactionDataCast
+    {
+        public int ID { get; set; }
+        public decimal Amount { get; set; }
+        public string Note { get; set; }
+        public DateTime TransactionDate { get; set; }            
+        public bool IsHidden { get; set; }
+        public int TransactionTypeID { get; set; }
+        public int TransactionCategoryID { get; set; }
+        public bool IsActive { get; set; }    
+        public string UserID { get; set; }
+    }
+}
